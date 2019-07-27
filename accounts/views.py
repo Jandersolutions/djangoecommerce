276219268528f5799e61df3d903207b827c4fd65
jandersolutions <jba1719@gmail.com>
2019-07-27 10:41:30 -0300

    edit views accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import CreateView
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from .forms import UserAdminCreationForm
 from .models import User
