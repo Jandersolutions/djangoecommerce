aa71408a0d1a06d7c974d800ef26c2c62e679e7f
jandersolutions <jba1719@gmail.com>
2019-07-27 15:39:15 -0300

    app checkout cart

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class CartItemManager(models.Manager):
+    def add_item(self, cart_key, product):
+        pass
+
+
 class CartItem(models.Model):
     cart_key = models.CharField(
         'Chave do Carrinho', max_length=40, db_index=True
@@ -9,9 +14,12 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField('Quantidade', default=1)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
 
+    objects = CartItemManager()
+
     class Meta:
         verbose_name = 'Item do Carrinho'
         verbose_name_plural = 'Itens dos Carrinhos'
+        unique_together = (('cart_key', 'product'),)
 
     def __str__(self):
         return '{} [{}]'.format(self.product, self.quantity)
