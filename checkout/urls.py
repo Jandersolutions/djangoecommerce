c4a1854f860a15b8e25b608d4dec037b0d6d55e4
jandersolutions <jba1719@gmail.com>
2019-07-28 23:26:26 -0300

    add page cart

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('carrinho/adicionar/<slug:slug>', views.create_cartitem, name='cart_item'),
+    path('carrinho/adicionar/<slug:slug>', views.create_cartitem, name='create_cartitem'),
+    path('carrinho/', views.cart_item, name='cart_item'),
 ]
